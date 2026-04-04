b6e5bb1a4750a559e3226883af6cd68add31fe25
Felipe Saraiva <felipeluizsaraiva71@gmail.com>
2026-04-04 02:04:05 +0000

    primeiro upload do site Panda Ar-condicionado.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Scroll suave
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener("click", function(e) {
     e.preventDefault();
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth"
